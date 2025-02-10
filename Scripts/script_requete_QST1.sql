a6628b640340869d41e6764d1f4b82335e8e7765
YounesHitmi <younes.hitmi@etu.unice.fr>
2025-02-10 12:21:27 +0100

    correction requete qst1 duplication qst3

--- Scripts/script_requete_QST1.sql
+++ Scripts/script_requete_QST1.sql
@@ -1,15 +1,39 @@
-SELECT 
-    gare.idGare AS id_gare, 
-    COUNT(*) AS nombre_trains,
-    g.nbQuais * 4 * 4 AS capacite_max, 
-    (COUNT(*) * 100.0 / (g.nbQuais * 4 * 4)) AS taux_saturation
-FROM (
-    SELECT idGareDepart AS idGare, heure FROM Trajets
-    WHERE (EXTRACT(HOUR FROM heure) BETWEEN 7 AND 9) OR (EXTRACT(HOUR FROM heure) BETWEEN 17 AND 19)
+WITH RECURSIVE Chemins AS (
+    -- Étape 1 : Récupérer les trajets directs depuis la gare de départ
+    SELECT 
+        idTrain, 
+        idGareDepart, 
+        idGareArrivee, 
+        date, 
+        heure, 
+        duree,
+        0 AS nb_correspondances,  -- Pas de correspondance pour les trajets directs
+        duree AS duree_totale, 
+        ARRAY[idTrain] AS sequence_trains
+    FROM Trajets
+    WHERE idGareDepart = 1
+
     UNION ALL
-    SELECT idGareArrivee AS idGare, heure FROM Trajets
-    WHERE (EXTRACT(HOUR FROM heure) BETWEEN 7 AND 9) OR (EXTRACT(HOUR FROM heure) BETWEEN 17 AND 19)
-) AS gare
-JOIN Gares g ON gare.idGare = g.idGare
-GROUP BY gare.idGare, g.nbQuais
-ORDER BY taux_saturation DESC;
+
+    -- Étape 2 : Récursivement, ajouter des correspondances en minimisant les attentes
+    SELECT 
+        t.idTrain, 
+        c.idGareDepart, 
+        t.idGareArrivee,
+        t.date, 
+        t.heure, 
+        t.duree, 
+        c.nb_correspondances + 1 AS nb_correspondances, 
+        c.duree_totale + t.duree  + (EXTRACT(EPOCH FROM (t.heure - (c.heure + c.duree))) * INTERVAL '1 second') AS duree_totale,
+        c.sequence_trains || t.idTrain AS sequence_trains
+    FROM Trajets t
+    JOIN Chemins c ON t.idGareDepart = c.idGareArrivee
+    WHERE t.heure > (c.heure + c.duree)  -- Vérifie qu'on ne rate pas la correspondance
+	AND c.nb_correspondances < 3  -- Limite le nombre de correspondances
+    AND NOT t.idTrain = ANY(c.sequence_trains)  -- Évite les boucles infinies
+)
+SELECT * 
+FROM Chemins
+WHERE idGareArrivee = 8
+ORDER BY nb_correspondances ASC, duree_totale ASC
+LIMIT 1;
